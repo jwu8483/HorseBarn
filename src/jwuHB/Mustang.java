014e0c46de76be8d9d3df5bdf41662e63e5cb131
jwu8483 <43213089+jwu8483@users.noreply.github.com>
2018-10-25 10:53:17 -0400

    Update Mustang.java

--- src/jwuHB/Mustang.java
+++ src/jwuHB/Mustang.java
@@ -1,4 +1,36 @@
 package jwuHB;
 
-public class Mustang {
+public class Mustang implements Horse{
+
+    private String name;
+    private int weight;
+
+    public Mustang(String name, int weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String toString()
+    {
+        return name;
+    }
 }
